5343d2e3ec70bb1bce1712fa33d04933703fabb5
sihena <78011469+sihena@users.noreply.github.com>
2021-02-18 23:41:25 +0900

    JS:Navbar menu

--- js/main.js
+++ js/main.js
@@ -10,3 +10,21 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--dark');
   }
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (e) => {
+  const target = e.target;
+  const link = target.dataset.link;
+  if (link === null) {
+    return;
+  }
+  navbarMenu.classList.remove('open');
+  scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
