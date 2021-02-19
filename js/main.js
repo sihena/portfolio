305d3c65ee34b020c839ac765a4f63289db8fc8c
sihena <78011469+sihena@users.noreply.github.com>
2021-02-19 16:18:07 +0900

    JS:Scroll contents

--- js/main.js
+++ js/main.js
@@ -23,17 +23,74 @@ navbarMenu.addEventListener('click', (e) => {
   scrollIntoView(link);
 });
 
+
+// 모든 색션의 요소들과 메뉴 아이템들을 가져옴
+const sectionIds = [
+  '#home',
+  '#about',
+  '#skills',
+  '#work',
+  '#contact'
+];
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id => document.querySelector(`[data-link="${id}"]`));
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+  selectedNavItem.classList.remove('active');
+  selectedNavItem = selected;
+  selectedNavItem.classList.add('active');
+}
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({behavior: "smooth"});  
+  selectNavItem(navItems[sectionIds.indexOf(selector)]);  
+}
+
+const observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.3,
+};
+
+const observerCallback = (entries, observer) => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      // 스크롤링이 아래로 되어서 페이지가 올라옴
+      if (entry.boundingClientRect.y < 0) {
+        selectedNavIndex = index + 1;
+      } else {
+        selectedNavIndex = index - 1;
+      }
+    }
+  });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach(section => observer.observe(section));
+
+window.addEventListener('wheel', () => {
+  if(window.scrollY === 0) {
+    selectedNavIndex = 0;
+  } else if (Math.round(window.scrollY + window.innerHeight) >= document.body.clientHeigh) {
+    selectedNavIndex = navItems.length -1;
+  }
+  selectNavItem(navItems[selectedNavIndex]);
+});
+
+
 // Navbar toggle button for small screen
 const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
 navbarToggleBtn.addEventListener('click', () => {
   navbarMenu.classList.toggle('open');
 });
 
-
-
 // Handle click on "contact me" button on home
-const contactMe = document.querySelector('.home__contact');
-contactMe.addEventListener('click', (e) => {
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
   scrollIntoView('#contact');
 });
 
@@ -59,25 +116,27 @@ arrowUp.addEventListener('click', () => {
   scrollIntoView('#home');
 });
 
-// Project
+// Projects
 const workBtnContainer = document.querySelector('.work__categories');
 const projectContainer = document.querySelector('.work__projects');
 const projects = document.querySelectorAll('.project');
-workBtnContainer.addEventListener('click', (e) => {
+workBtnContainer.addEventListener('click', e => {
   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
-  if (filter === null) {
+  if (filter == null) {
     return;
   }
 
-  // Remove slrction from the previous item and select the new one
+  // Remove selection from the previous item and select the new one
   const active = document.querySelector('.category__btn.selected');
-  active.classList.remove('selected');
-  const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
-  target.classList.add('selected');
+  if (active != null) {
+    active.classList.remove('selected');
+  }
+  e.target.classList.add('selected');
 
   projectContainer.classList.add('anim-out');
   setTimeout(() => {
-    projects.forEach((project) => {
+    projects.forEach(project => {
+      console.log(project.dataset.type);
       if (filter === '*' || filter === project.dataset.type) {
         project.classList.remove('invisible');
       } else {
@@ -91,8 +150,3 @@ workBtnContainer.addEventListener('click', (e) => {
 
 
 
-
-function scrollIntoView(selector) {
-  const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({behavior: "smooth"});    
-}
