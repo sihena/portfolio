b35adbcb27e8f7d6cebba1b0a509c3f2b71a8055
sihena <78011469+sihena@users.noreply.github.com>
2021-02-22 23:35:42 +0900

    modify-1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
@@ -10,6 +11,7 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--dark');
   }
 });
+
 
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
@@ -88,11 +90,13 @@ navbarToggleBtn.addEventListener('click', () => {
   navbarMenu.classList.toggle('open');
 });
 
+
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', () => {
   scrollIntoView('#contact');
 });
+
 
 // Make home slowly fade to transparent as the window scrolls down
 const home = document.querySelector('.home__container');
@@ -100,6 +104,7 @@ const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
 
 // Show "arrow up" button when scrolling down
 const arrowUp = document.querySelector('.arrow-up');
@@ -111,20 +116,23 @@ document.addEventListener('scroll', () => {
   }
 });
 
+
 // Handle click on the "arrow up" button
 arrowUp.addEventListener('click', () => {
   scrollIntoView('#home');
 });
 
+
 // Projects
 const workBtnContainer = document.querySelector('.work__categories');
 const projectContainer = document.querySelector('.work__projects');
 const projects = document.querySelectorAll('.project');
-workBtnContainer.addEventListener('click', e => {
+workBtnContainer.addEventListener('click', (e) => {
   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
-  if (filter == null) {
+  if (filter === null) {
     return;
   }
+
 
   // Remove selection from the previous item and select the new one
   const active = document.querySelector('.category__btn.selected');
@@ -146,7 +154,3 @@ workBtnContainer.addEventListener('click', e => {
     projectContainer.classList.remove('anim-out');
   }, 300);
 });
-
-
-
-
